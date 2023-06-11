6f52c39a7cd2049e12c004a5bb188f0f6df935e0
Anshika <anshika2011087@akgec.ac.in>
2023-06-11 15:29:21 +0530

    greedy

---
+++ Max_Chain.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+                  
+//(Anshika Agarwal)                   
+public class Max_Chain  {
+                   
+     public static void main(String args[]) throws IOException {
+                   
+int pairs[][] ={{5,24}, {39,60},{5,28},{27,40},{50,90}};
+Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+int chainLen =1;
+int chainEnd = pairs[0][1];
+
+for(int i=1;i<pairs.length;i++)
+{
+    if(pairs[i][0] > chainEnd)
+    {
+        chainLen++;
+        chainEnd = pairs[i][1];
+    }
+}
+System.out.println("Max length of chain = "+chainLen);
+     }
+                                    
+                                     
+      //O(nlogn) complexity                             
+                  
+                   
+//#########################################################################################################
+    static int Intmax(){return Integer.MAX_VALUE;} 
+    static int Intmint(){ return Integer.MIN_VALUE;}
+    static char get(String s, int i) {char c=s.charAt(i);return c;}
+    static int max(int a, int b){return Math.max(a, b);}
+    static int min(int a, int b){return Math.min(a, b);}
+static void print(int a[]){int n=a.length;for(int i=0;i<n;i++){System.out.print(a[i]);}}
+    //FastReader
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        String nextLine()
+        {
+        String str = ""; 
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken(
+);
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        //Input Array elements
+        int[] intArray(int n){
+            int[] arr = new int[n];
+            for(int i =0; i < n; i ++){
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+        //Input ArrayList elements
+        ArrayList<Integer> intA(int n){
+            ArrayList<Integer> a=new ArrayList<>();
+            for(int i =0; i < n; i ++){
+                a.add(nextInt());
+            }
+            return a;
+        }
+            }
+        } 
