3ed938ec08bd668e0c00e3244266d719970bc46b
Anshika <anshika2011087@akgec.ac.in>
2023-06-12 21:46:19 +0530

    greedy

---
+++ Job_seq.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.util.*;
+                  
+//(Anshika Agarwal)                   
+public class  Job_seq {
+    static class Job{
+        int deadLine;
+        int profit;
+        int id;
+
+        public Job(int i, int d, int p)
+        {
+            id=i;
+            deadLine=d;
+            profit=p;
+        }
+    
+                   
+     public static void main(String args[]) throws IOException {
+                   
+        int jobsInfo[][] = {{4,20},{1,10},{1,40},{1,30}};
+
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        for(int i=0;i<jobsInfo.length;i++)
+        {
+            jobs.add(new Job(i,jobsInfo[i][0],jobsInfo[i][1]));
+        }
+        Collections.sort(jobs, (obj1 , obj2) -> obj2.profit-obj1.profit);
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time=0;
+        for(int i=0;i<jobs.size();i++)
+        {
+            Job curr = jobs.get(i);
+            if(curr.deadLine>time){
+                seq.add(curr.id);
+                time++;
+            }
+        }
+        System.out.println("max jobs = "+seq.size());
+        for(int i=0;i<seq.size();i++)
+        {
+            System.out.print(seq.get(i)+" ");
+        }
+        System.out.println();
+     }
+    }
+                                    
+                                     
+                                   
+                  
+                   
+//#########################################################################################################
+    static int Intmax(){return Integer.MAX_VALUE;} 
+    static int Intmint(){ return Integer.MIN_VALUE;}
+    static char get(String s, int i) {char c=s.charAt(i);return c;}
+    static int max(int a, int b){return Math.max(a, b);}
+    static int min(int a, int b){return Math.min(a, b);}
+static void print(int a[]){int n=a.length;for(int i=0;i<n;i++){System.out.print(a[i]);}}
+    //FastReader
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        String nextLine()
+        {
+        String str = ""; 
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken(
+);
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        //Input Array elements
+        int[] intArray(int n){
+            int[] arr = new int[n];
+            for(int i =0; i < n; i ++){
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+        //Input ArrayList elements
+        ArrayList<Integer> intA(int n){
+            ArrayList<Integer> a=new ArrayList<>();
+            for(int i =0; i < n; i ++){
+                a.add(nextInt());
+            }
+            return a;
+        }
+            }
+        }
